27ce1c06fdd69b036cfb39b2c9c365e79d6d48de
headsink <headsink@users.noreply.github.com>
2022-05-26 14:06:46 +0800

    Create gtag.js

---
+++ gtag.js
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-YVQG74K4JJ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-YVQG74K4JJ');
+</script>
